aba170aac006cd505fa5aa5844b847158359aab7
cmcalvain <colin.mcalvain@gmail.com>
2026-01-20 18:29:38 -0800

    Adds initial project code skeleton
    Sets up the basic structure for the Arduino project,
    including pin definitions, setup and loop functions,
    and placeholder functions for future validation and morse
    code implementation.
    
    Initializes pin modes and sets initial output states.

--- main.cpp
+++ main.cpp
@@ -14,13 +14,12 @@ mcalvain colin      1/20/26         1.0.0               init
 ****************************************************************************************************/
 //includes
 #include <Arduino.h>
-#include <cstdint>
 
 //declarations
 void setup();
 void loop();
 void getString(char*);
-void flashLED() //ratio: signal/letter/word == 1/3/7 == 60ms/180ms/420ms
+void flashLED(); //ratio: signal/letter/word == 1/3/7 == 60ms/180ms/420ms
 
 /**validation function**/
 int myFunction(int, int);
@@ -29,27 +28,27 @@ void altLow();
 /**validation function**/
 
 //pin setup
-const uint8_t PIN_D0  = 0;
-const uint8_t PIN_D1  = 1;
-const uint8_t PIN_D2  = 2;
-const uint8_t PIN_D3  = 3;
-const uint8_t PIN_D4  = 4;
-const uint8_t PIN_D5  = 5;
-const uint8_t PIN_D6  = 6;
-const uint8_t PIN_D7  = 7;
-const uint8_t PIN_D8  = 8;
-const uint8_t PIN_D9  = 9;
-const uint8_t PIN_D10 = 10;
-const uint8_t PIN_D11 = 11;
-const uint8_t PIN_D12 = 12;
-const uint8_t PIN_D13 = 13;
+const uint8_t PIN_D_0  = 0;
+const uint8_t PIN_D_1  = 1;
+const uint8_t PIN_D_2  = 2;
+const uint8_t PIN_D_3  = 3;
+const uint8_t PIN_D_4  = 4;
+const uint8_t PIN_D_5  = 5;
+const uint8_t PIN_D_6  = 6;
+const uint8_t PIN_D_7  = 7;
+const uint8_t PIN_D_8  = 8;
+const uint8_t PIN_D_9  = 9;
+const uint8_t PIN_D_10 = 10;
+const uint8_t PIN_D_11 = 11;
+const uint8_t PIN_D_12 = 12;
+const uint8_t PIN_D_13 = 13;
 
-const uint8_t PIN_A0 = A0;
-const uint8_t PIN_A1 = A1;
-const uint8_t PIN_A2 = A2;
-const uint8_t PIN_A3 = A3;
-const uint8_t PIN_A4 = A4;
-const uint8_t PIN_A5 = A5;
+const uint8_t PIN_A_0 = A0;
+const uint8_t PIN_A_1 = A1;
+const uint8_t PIN_A_2 = A2;
+const uint8_t PIN_A_3 = A3;
+const uint8_t PIN_A_4 = A4;
+const uint8_t PIN_A_5 = A5;
 
 /*****************************************************************************************************
 FUNCTION NAME:  Setup()
@@ -61,39 +60,39 @@ NOTES:          TODO: overly done for learning. Will reduce to what is need late
 void setup() {
     Serial.begin(115200);
 
-    pinMode(PIN_D0, INPUT);
-    pinMode(PIN_D1, INPUT);
+    pinMode(PIN_D_0, INPUT);
+    pinMode(PIN_D_1, INPUT);
 
-    pinMode(PIN_D2, INPUT_PULLUP);
-    pinMode(PIN_D3, INPUT_PULLUP);
-    pinMode(PIN_D4, INPUT_PULLUP);
+    pinMode(PIN_D_2, INPUT_PULLUP);
+    pinMode(PIN_D_3, INPUT_PULLUP);
+    pinMode(PIN_D_4, INPUT_PULLUP);
 
-    pinMode(PIN_D5, OUTPUT);
-    pinMode(PIN_D6, OUTPUT);
-    pinMode(PIN_D7, OUTPUT);
-    pinMode(PIN_D8, OUTPUT);
-    pinMode(PIN_D9, OUTPUT);
-    pinMode(PIN_D10, OUTPUT);
-    pinMode(PIN_D11, OUTPUT);
-    pinMode(PIN_D12, OUTPUT);
-    pinMode(PIN_D13, OUTPUT);
+    pinMode(PIN_D_5, OUTPUT);
+    pinMode(PIN_D_6, OUTPUT);
+    pinMode(PIN_D_7, OUTPUT);
+    pinMode(PIN_D_8, OUTPUT);
+    pinMode(PIN_D_9, OUTPUT);
+    pinMode(PIN_D_10, OUTPUT);
+    pinMode(PIN_D_11, OUTPUT);
+    pinMode(PIN_D_12, OUTPUT);
+    pinMode(PIN_D_13, OUTPUT);
 
-    digitalWrite(PIN_D5, LOW);
-    digitalWrite(PIN_D6, LOW);
-    digitalWrite(PIN_D7, LOW);
-    digitalWrite(PIN_D8, LOW);
-    digitalWrite(PIN_D9, LOW);
-    digitalWrite(PIN_D10, LOW);
-    digitalWrite(PIN_D11, LOW);
-    digitalWrite(PIN_D12, LOW);
-    digitalWrite(PIN_D13, LOW);
+    digitalWrite(PIN_D_5, LOW);
+    digitalWrite(PIN_D_6, LOW);
+    digitalWrite(PIN_D_7, LOW);
+    digitalWrite(PIN_D_8, LOW);
+    digitalWrite(PIN_D_9, LOW);
+    digitalWrite(PIN_D_10, LOW);
+    digitalWrite(PIN_D_11, LOW);
+    digitalWrite(PIN_D_12, LOW);
+    digitalWrite(PIN_D_13, LOW);
 
-    pinMode(PIN_A0, INPUT);
-    pinMode(PIN_A1, INPUT);
-    pinMode(PIN_A2, INPUT);
-    pinMode(PIN_A3, INPUT);
-    pinMode(PIN_A4, INPUT);
-    pinMode(PIN_A5, INPUT);
+    pinMode(PIN_A_0, INPUT);
+    pinMode(PIN_A_1, INPUT);
+    pinMode(PIN_A_2, INPUT);
+    pinMode(PIN_A_3, INPUT);
+    pinMode(PIN_A_4, INPUT);
+    pinMode(PIN_A_5, INPUT);
 
     analogReference(DEFAULT);
 }
@@ -131,27 +130,27 @@ int myFunction(int x, int y) {
 
 /**validation function**/
 void altHigh(){
-    digitalWrite(PIN_D5, HIGH);
-    digitalWrite(PIN_D6, HIGH);
-    digitalWrite(PIN_D7, HIGH);
-    digitalWrite(PIN_D8, HIGH);
-    digitalWrite(PIN_D9, HIGH);
-    digitalWrite(PIN_D10, HIGH);
-    digitalWrite(PIN_D11, HIGH);
-    digitalWrite(PIN_D12, HIGH);
-    digitalWrite(PIN_D13, HIGH);
+    digitalWrite(PIN_D_5, HIGH);
+    digitalWrite(PIN_D_6, HIGH);
+    digitalWrite(PIN_D_7, HIGH);
+    digitalWrite(PIN_D_8, HIGH);
+    digitalWrite(PIN_D_9, HIGH);
+    digitalWrite(PIN_D_10, HIGH);
+    digitalWrite(PIN_D_11, HIGH);
+    digitalWrite(PIN_D_12, HIGH);
+    digitalWrite(PIN_D_13, HIGH);
 }
 /**validation function**/
 /**validation function**/
 void altLow(){
-    digitalWrite(PIN_D5, LOW);
-    digitalWrite(PIN_D6, LOW);
-    digitalWrite(PIN_D7, LOW);
-    digitalWrite(PIN_D8, LOW);
-    digitalWrite(PIN_D9, LOW);
-    digitalWrite(PIN_D10, LOW);
-    digitalWrite(PIN_D11, LOW);
-    digitalWrite(PIN_D12, LOW);
-    digitalWrite(PIN_D13, LOW);
+    digitalWrite(PIN_D_5, LOW);
+    digitalWrite(PIN_D_6, LOW);
+    digitalWrite(PIN_D_7, LOW);
+    digitalWrite(PIN_D_8, LOW);
+    digitalWrite(PIN_D_9, LOW);
+    digitalWrite(PIN_D_10, LOW);
+    digitalWrite(PIN_D_11, LOW);
+    digitalWrite(PIN_D_12, LOW);
+    digitalWrite(PIN_D_13, LOW);
 }
 /**validation function**/
